216acde5ab7d7bd0dde0ac4403f92df3d9ede86c
adaptationio <willie@adaptatation.io>
2019-01-03 13:03:50 +1030

    refactor: main and requirements

---
+++ __main__.py
@@ -0,0 +1,16 @@
+import argparse
+
+import gym
+import numpy as np
+
+
+def main():
+    pass
+    
+    
+
+if __name__ == '__main__':
+    # execute only if run as the entry point into the program
+    main()
+    
+
